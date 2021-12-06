374bee401ae2c3f6eebaa3fb0b3f87e81c6e26a9
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-12-06 14:54:20 +0700

    Lấy dữ liệu từ json list object đơn sang

--- src/main/java/com/thuctap/struts2_crud_mybatis/action/KhoaAction.java
+++ src/main/java/com/thuctap/struts2_crud_mybatis/action/KhoaAction.java
@@ -3,6 +3,7 @@ package com.thuctap.struts2_crud_mybatis.action;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,7 +11,23 @@ import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Result;
 
+import com.google.gson.Gson;
+import com.thuctap.struts2_crud_mybatis.model.*;
+
 public class KhoaAction {
+    private List<Student> students;
+    
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+
     HttpServletResponse response = ServletActionContext.getResponse();
 
 
@@ -18,11 +35,28 @@ public class KhoaAction {
     public String viewKhoa() throws IOException{
         PrintWriter printWriter = response.getWriter();
         //response.setStatus(400);
-        printWriter.println("{\"message\":\"" + "Test message" + "\"}");
+       // printWriter.println("{\"message\":\"" + "Test message" + "\"}");
+
+       //Generate list to prepare convert to json by gson method
+       Student studenta = new Student();
+       studenta.setName("Thien");
+       studenta.setBranch("60cntt1");
+       studenta.setEmail("thien12@gmail.com");
+       studenta.setPercentage(90);
+       studenta.setPhone(8412471);
+
+       Gson gson = new Gson();
+       String json = gson.toJson(studenta); 
+
+       //Convert object to json
+       printWriter.print(json);
+       
+
         printWriter.flush();
         printWriter.close();
     
         return "success";
     }
+
     
 }
--- src/main/webapp/js/khoa.js
+++ src/main/webapp/js/khoa.js
@@ -1,14 +1,24 @@
 const formDOM = document.querySelector('#loginForm');
-const errorMsgDOM = document.querySelector('#error-msg');
+//const errorMsgDOM = document.querySelector('#error-msg');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const phone = document.querySelector('#phone');
+const percentage = document.querySelector('#percentage');
 
 
 formDOM.addEventListener('submit', async (event) => {
     event.preventDefault();
-    errorMsgDOM.textContent = '';
+    //errorMsgDOM.textContent = '';
       try{
+        //Include json page through Ajax
         const { data } = await axios.get('./khoa/index');
         console.log(data);
-        errorMsgDOM.textContent = data.message;
+        //errorMsgDOM.textContent = data.message;
+        //Return data from server to html
+        name.textContent = data.name;
+        email.textContent = data.email;
+        phone.textContent = data.phone;
+        percentage.textContent = data.percentage;
       }  
       catch(error) {
           //errorMsgDOM.textContent = error.response.data.message;
