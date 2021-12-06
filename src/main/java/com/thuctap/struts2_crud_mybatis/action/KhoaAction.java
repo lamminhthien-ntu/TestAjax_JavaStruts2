bbe747e8c26cb8a98e41bad1cdc5c411d21bc463
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-12-06 20:27:24 +0700

    Add files via upload
    Multi key value pair from JSON

--- src/main/java/com/thuctap/struts2_crud_mybatis/action/KhoaAction.java
+++ src/main/java/com/thuctap/struts2_crud_mybatis/action/KhoaAction.java
@@ -3,36 +3,14 @@ package com.thuctap.struts2_crud_mybatis.action;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Result;
 
-import mybatis.mapper.StudentMapper;
-
-import com.google.gson.Gson;
-import com.thuctap.struts2_crud_mybatis.db.ConnectDB;
-import com.thuctap.struts2_crud_mybatis.model.*;
-
 public class KhoaAction {
-    public List<Student> listStudents;
-    private SqlSessionFactory sqlSessionFactory = ConnectDB.getSqlSessionFactory();
-
-    public List<Student> getListStudents() {
-        return listStudents;
-    }
-
-
-    public void setListStudents(List<Student> listStudents) {
-        this.listStudents = listStudents;
-    }
-
-
     HttpServletResponse response = ServletActionContext.getResponse();
 
 
@@ -40,27 +18,16 @@ public class KhoaAction {
     public String viewKhoa() throws IOException{
         PrintWriter printWriter = response.getWriter();
         //response.setStatus(400);
-       // printWriter.println("{\"message\":\"" + "Test message" + "\"}");
-        
-       // Mở Session
-        SqlSession session = sqlSessionFactory.openSession();
-
-        StudentMapper studentMapper = session.getMapper(StudentMapper.class);
-
-        listStudents = studentMapper.getAll();
-       //Convert object to json
-       Gson gson = new Gson();
-       String json = gson.toJson(listStudents); 
-
-       printWriter.print(json);
-
-       System.out.print(json);
-       
+        // {
+        //     "a": "thien",
+        //     "b": "nam",
+        //     "c": "ga"
+        //   }
+        printWriter.print("{\"a\": \"thien\", \"b\": \"nam\", \"c\": \"ga\"}");
         printWriter.flush();
         printWriter.close();
     
         return "success";
     }
-
     
 }
--- src/main/webapp/js/khoa.js
+++ src/main/webapp/js/khoa.js
@@ -1,22 +1,18 @@
 const formDOM = document.querySelector('#loginForm');
-const studentListDOM = document.querySelector('#student-list');
+const errorMsgDOM = document.querySelector('#error-msg');
+
 
 formDOM.addEventListener('submit', async (event) => {
     event.preventDefault();
+    errorMsgDOM.textContent = '';
       try{
-        //Include json page through Ajax
         const { data } = await axios.get('./khoa/index');
-        //console.log(data);
-        const allStudent = data.map((student) => {
-          const {name,id,email,branch,percentage,phone} = student;
-          return `
-            <p>${name} ${id} ${branch} ${percentage} ${email} ${phone}<p>
-          `
-        }).join(' '); 
-        studentListDOM.innerHTML = allStudent;
-      }
+        console.log(data);
+        console.log(data.a);
+        console.log(data.b);
+        console.log(data.c);
+      }  
       catch(error) {
           //errorMsgDOM.textContent = error.response.data.message;
       }
-      
 });
