18e96ebd6de28b190da69db299ec8982d8e84b0f
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-12-07 22:19:36 +0700

    fix lại login = cookie, js và ajax

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,28 +1,36 @@
+const checkLogined = async () => {
+    try {
+        await axios.post(`./login`);
+        window.location.href = "./login";
+    }
+    catch (error) {
+        
+    }
+}
+
 const validateLoginForm = async () => {
+    //Hiên thị thông báo lỗi
     const loginErrorMessage = document.querySelector('#login_error');
     const passwordErrorMessage = document.querySelector('#password_error');
+
+    //Lấy dữ liệu usernmae và password
     const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
+    //Thực hiện request
     try {
          await axios.post(`./login?username=${username}&password=${password}`);
          window.location.href = "./login";
-        //console.log("123");
-        //console.log(data);
-
     } catch (error) {
-       if (typeof(error.response.data) != "undefined")
-       {
+        //Khi có lỗi thì 
            const data = error.response.data;
             console.log(data);
             loginErrorMessage.innerHTML=data[0];
             passwordErrorMessage.innerHTML=data[1];
-       }
-       
-            
+     
     }
 }
-// function validateLoginForm(){
-//     console.log("abc");
-// }
+
+//Luôn luôn chạy hàm kiểm tra đã đăng nhập hay chưa (đã có session chưa), để tự redirect sang trang index (list)
+checkLogined();
 
 
